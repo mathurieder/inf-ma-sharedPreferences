09e1ca6209a0f9dced5cdd4d736b54628afd9ebe
unknown <nriedm@NB-NRIEDM.bbcnet.ch>
2015-06-19 15:09:16 +0200

    Initial commit

---
+++ app/src/main/java/ch/berufsbildungscenter/demosharedpreferences/SettingActivity.java
@@ -0,0 +1,23 @@
+package ch.berufsbildungscenter.demosharedpreferences;
+
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class SettingActivity extends PreferenceActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getFragmentManager().beginTransaction().replace(android.R.id.content, new PreferenceFragment()
+        {
+            public void onCreate(Bundle savedInstanceState)
+            {
+                super.onCreate(savedInstanceState);
+                addPreferencesFromResource(R.xml.settings);
+            }
+        }).commit();
+
+    }
+}
